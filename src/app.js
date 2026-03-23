b3fb8c9238a010758981fd44c1e0074d7b46f029
Daniel Castillo <116021248+CodingwCastle@users.noreply.github.com>
2026-03-23 23:58:30 +0000

    randomCardGenerator v3

--- src/app.js
+++ src/app.js
@@ -9,19 +9,22 @@ window.onload = function() {
   let suits = ["♦", "♥", "♠", "♣"];
   let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-  let randomSuitIndex = Math.floor(Math.random() * suits.length);
-  let randomValueIndex = Math.floor(Math.random() * values.length);
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  let randomValue = Math.floor(Math.random() * values.length);
 
-  let finalSuit = suits[randomSuitIndex];
-  let finalValue = values[randomValueIndex];
+  let finalSuit = suits[randomSuit];
+  let finalValue = values[randomValue];
 
   document.getElementById("top-suit").innerHTML = finalSuit;
-  document.getElementById("number").innerHTML = finalValue;
+  document.getElementById("Value").innerHTML = finalValue;
   document.getElementById("bottom-suit").innerHTML = finalSuit;
 
   if (finalSuit === "♥" || finalSuit === "♦") {
-    document.getElementById("card-container").style.color = "red";
-};
+  card.style.color = "red";
+} else {
+  card.style.color = "white";
+}
+
   document.getElementById("getANewCardBttn").addEventListener("click", function() {
     location.reload();
   });
